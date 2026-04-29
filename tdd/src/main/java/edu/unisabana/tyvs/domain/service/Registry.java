b44b4803906eaf6369c1cf7909b6bbd321f516ad
Manuel-Carreno <mdcarreno424@gmail.com>
2026-04-29 18:54:23 -0500

    requerimientos y archivos principales

---
+++ tdd/src/main/java/edu/unisabana/tyvs/domain/service/Registry.java
@@ -0,0 +1,11 @@
+package edu.unisabana.tyvs.domain.service;
+
+import edu.unisabana.tyvs.domain.model.*;
+
+public class Registry {
+
+    public RegisterResult registerVoter(Person p) {
+        // TODO Validate person and return real result.
+        return RegisterResult.VALID;
+    }
+}
